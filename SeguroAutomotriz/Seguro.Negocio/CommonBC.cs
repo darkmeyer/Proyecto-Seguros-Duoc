1a6c107d400392230bbcd0f6785ec49e23aef60e
darkmeyer <darkmeyer@gmail.com>
2017-04-15 22:29:32 -0400

    1

---
+++ SeguroAutomotriz/Seguro.Negocio/CommonBC.cs
@@ -0,0 +1,32 @@
+﻿using Seguro.DALC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seguro.Negocio
+{
+    public class CommonBC
+    {
+        private static SeguroEntities _modeloSeguro;
+
+        public SeguroEntities ModeloSeguro
+        {
+            get 
+            { 
+                if(_modeloSeguro == null)
+                {
+                    _modeloSeguro = new SeguroEntities();
+                }
+                return _modeloSeguro; 
+            }
+        }
+
+        public CommonBC()
+        {
+
+        }
+        
+    }
+}
